9f07a4cf1204a7bbc6062c3f64658bfa16ca4087
Anshika Bhatt <anshikabhatt0207@gmail.com>
2021-05-25 20:03:10 +0530

    Send email on user acc

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
 const nodemailer = require("nodemailer");
-
+require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.urlencoded({
@@ -49,23 +49,24 @@ app.post("/", function(req, res) {
           console.log("disid is : " + disid);
 
           var today = new Date();
-          var da = String(today.getDate() + 1).padStart(2, '0');
+          var da = String(today.getDate()).padStart(2, '0');
           var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
           var yyyy = today.getFullYear();
 
           today = da + '-' + mm + '-' + yyyy;
           console.log(today);
           let url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=" + disid + "&date=" + today;
+          console.log(url);
           https.get(url, function(response3) {
             response3.on("data", function(data) {
-              let dw = JSON.parse(data);
-              let ses = dw.sessions;
+              const dw = JSON.parse(data);
+              const ses = dw.sessions;
               if (ses.length > 0) {
                 var transporter = nodemailer.createTransport({
                   service: 'gmail',
                   auth: {
-                    user: 'anshikabhatt0207@gmail.com',
-                    pass: 'anshi@g33'
+                    user: process.env.EMAIL,
+                    pass: process.env.PASSWORD
                   }
                 });
 
@@ -88,7 +89,6 @@ app.post("/", function(req, res) {
           });
         });
       });
-
     });
   });
 
